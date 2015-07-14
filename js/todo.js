f3c55995a00971828447afca6c036f0a1a8baf60
adi823 <adi823>
2015-07-14 22:14:44 +0200

    added localstorage support, separation of persistence layer done

--- js/todo.js
+++ js/todo.js
@@ -17,16 +17,48 @@ todoData = {
  	},
  	/** creates todo */
  	create: function(title, state) {
+         var taskLabelText = $('.newTask').val();
+         // Update data model
+		 var todo = {
+		     title: taskLabelText,
+		     state: "TODO"
+		 };
+		     todos.push(todo);
+		     console.debug('TODO array %o', todos);
+		     localStorage.setItem("todos", JSON.stringify(todos));
+		     updateView();
 
  	},
+
  	read: function(index) {
+
+ 		
 
  	},
  	update: function(index, todo) {
 
- 	},
+ 		 $(".list").empty();
+    	 for (var i=0; i<todos.length; i++) {
+    	 renderTask(todos[i]);
+		}
+ 		   
+	},
+ 
+	
  	delete: function(index) {
 
+ 		var index = $(this).parent().prevAll('.task').length;
+    	console.debug("delete entry %d", index);
+    	if (index < 0 || index >= todos.length) {
+        console.warn('index out of bounds');
+        return;
+    }
+   		todos.splice(index, 1);
+   		storeTodos(todos);
+
+    	//$(this).parent().remove();
+   		updateView();
  	}
+
 };
 
